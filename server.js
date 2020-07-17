1fb69b6217d8805c9357abd7842fb346796a1e17
Rajnil Prasad <rajnilprasad.rp@gmail.com>
2020-07-16 19:07:26 -0700

    initial commit

--- server.js
+++ server.js
@@ -17,12 +17,19 @@ app.use(express.static("public"));
 app.use(
     session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
 );
+
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 app.use(passport.initialize());
 app.use(passport.session());
 
 // Requiring our routes
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
+
 
 // Syncing our database and logging a message to the user upon success
 db.sequelize.sync().then(() => {
