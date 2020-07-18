ef3da443b732c70ed077f4394333aa9f3934661d
ashaw916 <a.s.young530@gmail.com>
2020-07-18 01:41:12 -0700

    added calendar, get routes for 3 main tables, style, started on calendar partial, non functional parts of server.js

--- models/businesses.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(sequelize, DataTypes) {
-    const Businesses = sequelize.define("Businesses", {
-     link_title: DataTypes.STRING,
-     link_text: DataTypes.STRING,
-
-    });
-    return Businesses;
-  };
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,32 +1,35 @@
 // Requiring path to so we can use relative routes to our HTML files
 const path = require("path");
-const db = require("../models")
+const db = require("../models");
 // Requiring our custom middleware for checking if a user is logged in
 const isAuthenticated = require("../config/middleware/isAuthenticated");
 
 module.exports = function(app) {
   app.get("/", (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect("/members");
-    }
     // database queries
-db.Protest.findAll().then(function(dateResults) {
-  db.Stories.findAll().then(function(articleResults) {
-    // db.links.findAll().then(function(linkResults) {
-      const dataObj = {dateResults, articleResults};
-      console.log("dataObj" + JSON.stringify(dataObj));
-      res.render("index", {datesObj : dataObj});
-      // res.render("index", {articlesObj : dataObj})
-      // res.render("index", {linksObj : dataObj})
-    // });
-  });
-});
-
-
-
-
-
+    db.Events.findAll().then(function(eventResults, err) {
+      if (err) {
+        throw err
+      }
+      console.log("events");
+      db.Articles.findAll().then(function(articleResults, err) {
+        if (err) {
+          throw err
+        }
+        console.log("articles");
+        db.Links.findAll().then(function(linkResults, err) {
+          console.log("links");
+            if (err) {
+              throw err
+            }
+          const dataObj = { eventResults, articleResults, linkResults };
+          console.log("dataObj" + JSON.stringify(dataObj));
+          res.render("index", { eventsObj: dataObj });
+          res.render("index", { articlesObj: dataObj });
+          res.render("index", { linksObj: dataObj });
+        }).catch();
+      }).catch();
+    }).catch();
   });
 
   app.get("/login", (req, res) => {
@@ -36,7 +39,13 @@ db.Protest.findAll().then(function(dateResults) {
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
-
+  app.get("/signup", (req, res) => {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/members");
+    }
+    res.sendFile(path.join(__dirname, "../public/signup.html"));
+  });
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, (req, res) => {
